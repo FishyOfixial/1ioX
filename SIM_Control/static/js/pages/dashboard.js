3f57afd0c15efab39bd47ea8c3b6ecfaf8f250f4
Fishy Ofixial <ivanrdlt47@gmail.com>
2025-07-11 22:37:17 -0600

    Agregar boton de refrescar tabla de uso mensual

--- SIM_Control/static/js/pages/dashboard.js
+++ SIM_Control/static/js/pages/dashboard.js
@@ -144,3 +144,18 @@ monthlyBtn.addEventListener("click", () => {
             document.getElementById("overlay").style.display = "none";
         });
 });
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.startsWith(name + "=")) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
