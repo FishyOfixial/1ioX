3550be0283246466d55553312f7930644f1ad542
Fishy Ofixial <ivanrdlt47@gmail.com>
2025-07-14 12:46:35 -0600

    .

--- staticfiles/js/pages/user_details.js
+++ staticfiles/js/pages/user_details.js
@@ -34,3 +34,30 @@ function togglePassword(btn) {
         btn.textContent = 'Mostrar';
     }
 }
+
+function confirmAction(action, isActive = null) {
+    const action_form = document.getElementById("action-form");
+    const text = document.getElementById('text-form')
+    const overlay = document.getElementById('overlay')
+    const action_input = document.getElementById("accionInput");
+
+    if (action === 'Cancel') {
+        overlay.style.display = "none";
+        action_form.style.display = "none";
+        return;
+    }
+
+    overlay.style.display = 'flex';
+    action_form.style.display = 'flex';
+
+    if (action === "Delete") {
+        text.innerHTML = "<strong>¿Estás seguro de que deseas eliminar al usuario?</strong><br>Esta acción no se puede deshacer.";
+        action_input.value = "delete";
+    } else if (action === "Active") {
+        const activar = isActive === "False" || isActive === false;
+        text.innerHTML = activar
+            ? "<strong>¿Estás seguro de que deseas activar al usuario?</strong><br>Esto permitirá que pueda iniciar sesión nuevamente."
+            : "<strong>¿Estás seguro de que deseas desactivar al usuario?</strong><br>Esta acción impedirá que inicie sesión.";
+        action_input.value = "active"
+    }
+}
