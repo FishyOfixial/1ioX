03f7d5253426e533d57c0aa237b60046c3623ac4
Fishy Ofixial <ivanrdlt47@gmail.com>
2025-07-13 20:24:40 -0600

    .

--- staticfiles/js/pages/sim_details.js
+++ staticfiles/js/pages/sim_details.js
@@ -3,7 +3,26 @@ const data_used = JSON.parse(document.getElementById('data-used').textContent);
 const sms_volume = JSON.parse(document.getElementById('sms-volume').textContent);
 const sms_used = JSON.parse(document.getElementById('sms-used').textContent);
 const monthly_use = JSON.parse(document.getElementById("monthly-use").textContent);
+const label_form = document.getElementById('label-form');
+const overlay = document.getElementById('overlay')
+const inputs = label_form.querySelectorAll("input")
 
 const labels = monthly_use.map(item => item.month);
 const monthly_data = monthly_use.map(item => item.data_used);
 const monthly_sms = monthly_use.map(item => item.sms_used);
+
+
+function labelFormFunc() {
+    if (label_form.style.display === "none") {
+        label_form.style.display = "flex"
+        overlay.style.display = "block"
+    }
+    else {
+        label_form.style.display = "none"
+        overlay.style.display = "none"
+
+        inputs.forEach(input => {
+            input.value = ""
+        })
+    }
+}
