5693aace473194f20067581d1c98a7b883c1ed0c
FishyOfixial <ivanrdlt47@gmail.com>
2025-09-06 01:03:29 -0600

    .

--- SIM_Control/signals.py
+++ SIM_Control/signals.py
@@ -1,40 +1,49 @@
-from django.db.models.signals import post_save
-from .models import Cliente
-from django.dispatch import receiver
+import threading
 from django.core.mail import send_mail
 import os
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Cliente
 
-SENDER_EMAIL = os.environ.get('SENDER_EMAIL')
+def send_email_async(subject, message, from_email, recipient_list):
+    send_mail(subject, message, from_email, recipient_list, fail_silently=False)
 
 @receiver(post_save, sender=Cliente)
 def send_welcome_email(sender, instance, created, **kwargs):
     if created:
-        password = (instance.last_name[:2] + instance.first_name[:2] + instance.phone_number[-4:])
-        send_mail(
-            subject='¡Se ha registrado a un nuevo cliente!',
-            message = f"""
-            Se ha registrado a {instance.get_full_name()}
-            Empresa: {instance.company}
-            Ciudad: {instance.city}, {instance.state}
-            Teléfono: {instance.phone_number}
-            Correo: {instance.email}
-            Contraseña: {password}
+        password = instance.last_name[:2] + instance.first_name[:2] + instance.phone_number[-4:]
+        
+        # Correo al administrador
+        threading.Thread(
+            target=send_email_async,
+            args=(
+                '¡Se ha registrado a un nuevo cliente!',
+                f"""
+                Se ha registrado a {instance.get_full_name()}
+                Empresa: {instance.company}
+                Ciudad: {instance.city}, {instance.state}
+                Teléfono: {instance.phone_number}
+                Correo: {instance.email}
+                Contraseña: {password}
                 """,
-            from_email=None,
-            recipient_list=[os.environ.get('SENDER_EMAIL')],
-            fail_silently=False
-        )
+                None,
+                [os.environ.get('SENDER_EMAIL')]
+            )
+        ).start()
 
-        send_mail(
-            subject="¡Haz sido registrado en 1iox!",
-            message = f"""
-            Hola, {instance.get_full_name()} haz sido registrado en la plataforma de 1iox.
+        # Correo al cliente
+        threading.Thread(
+            target=send_email_async,
+            args=(
+                '¡Haz sido registrado en 1iox!',
+                f"""
+                Hola, {instance.get_full_name()} haz sido registrado en la plataforma de 1iox.
 
-            Tu información de inicio de sesión
-            Usuario: {instance.email}
-            Contraseña: {password}
-            """,
-            from_email=None,
-            recipient_list=[instance.email],
-            fail_silently=False
-        )
+                Tu información de inicio de sesión
+                Usuario: {instance.email}
+                Contraseña: {password}
+                """,
+                None,
+                [instance.email]
+            )
+        ).start()
